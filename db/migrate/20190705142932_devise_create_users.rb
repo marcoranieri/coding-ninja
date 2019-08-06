52b6a83da38aca8a03aaaa3b8bf99d2540c8639e
Marco Ranieri <mr.marcoranieri@gmail.com>
2019-08-06 16:24:58 +0100

    SERVICE: class ApiFetchCompletedChallenges + api_key:string in User table

--- db/migrate/20190705142932_devise_create_users.rb
+++ db/migrate/20190705142932_devise_create_users.rb
@@ -34,6 +34,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
 ####### CUSTOM FIELDS
       t.string   :nickname
+      t.string   :codewars_api_key
       t.string   :codewars_username
       t.string   :photo
       t.boolean  :admin, null: false, default: false
